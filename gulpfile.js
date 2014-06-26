f13c2ee9c1a1081fb6903ab5595dd327cab57763
Dave Osborn <dave@osbornmultimedia.com>
2014-06-26 16:10:34 +0100

    First version of responsive css, with 3 page examples

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var compass = require('gulp-compass');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
 //var uglify = require('gulp-uglify');
 //var jshint = require('gulp-jshint');
 //var concat = require('gulp-concat');
@@ -37,10 +39,24 @@ gulp.task('js', function () {
       .pipe(gulp.dest(buildpath+'js'));
 });
 
+//gulp.task('watch', function() {
+    //gulp.watch('dev/js/*.js', ['js']);
+    //gulp.watch('dev/scss/**/*.scss', ['compass']);
+    //gulp.watch('dev/images/**/*', ['images']);
+//});
+
 gulp.task('watch', function() {
-    gulp.watch('dev/js/*.js', ['js']);
-    gulp.watch('dev/scss/**/*.scss', ['compass']);
-    gulp.watch('dev/images/**/*', ['images']);
+
+  gulp.watch('dev/scss/**/*.scss', ['compass']);
+
+  // Create LiveReload server
+  //var server = livereload();
+
+  // Watch any files in dist/, reload on change
+  //gulp.watch(['css/**/*']).on('change', function(file) {
+    //server.changed(file.path);
+  //});
+
 });
 
 gulp.task('default', function () {
